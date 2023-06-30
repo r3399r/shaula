5903dafc75f393a4ce6faadbc9d992fddddc6512
Yue <yue@potentia.asia>
2023-06-30 22:48:08 +0800

    classify usage

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,6 +5,7 @@ env=$1
 project=shaula
 # subDomain=wedding
 # domain=celestialstudio.net
+s3bucket=y-cf-midway-singapore # line2-cf-midway
 
 echo ====================================================================================
 echo env: $env
@@ -13,8 +14,9 @@ echo project: $project
 echo ====================================================================================
 
 echo deploy backend AWS...
+cd ../backend
 npm i
 npm run pre:deploy
-aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket line2-cf-midway
-aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket line2-cf-midway --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket $s3bucket
+aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket $s3bucket --capabilities CAPABILITY_NAMED_IAM
 echo ====================================================================================
