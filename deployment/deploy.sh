674d8d09560091962c2edddf9bc7bde0b7a4c133
Yue <yue@potentia.asia>
2023-07-01 00:07:01 +0800

    add deployment process of frontend

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -20,3 +20,11 @@ npm run pre:deploy
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket $s3bucket
 aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket $s3bucket --capabilities CAPABILITY_NAMED_IAM
 echo ====================================================================================
+
+echo deploy frontend to S3...
+cd ../frontend
+npm i
+npm run pre:deploy
+aws s3 sync ./dist s3://$project-$env-frontend --delete --cache-control no-cache
+# http://shaula-dev-frontend.s3-website-ap-southeast-1.amazonaws.com/
+echo ====================================================================================
